8065954087e61733daf20a90efe67d4f14b7bc14
ElRusioCG <carlos.gonzalez.erazo@gmail.com>
2025-07-31 17:11:31 -0400

    Add files via upload

---
+++ patron-nave-vercel/src/main.jsx
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+import './index.css';
+
+const App = () => <h1 className="text-2xl text-blue-700 p-4">Simulador Patrón de Nave Menor</h1>;
+
+ReactDOM.createRoot(document.getElementById('root')).render(<App />);
